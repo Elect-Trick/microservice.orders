560d0a1d8751a11c9937fd5c04a0df2f5a914cda
Elect-Trick <snmajenje@gmail.com>
2026-07-06 20:55:41 +0200

    Updates to Program, added routing,authentication,authorization and controllers

--- OrdersAPI/Program.cs
+++ OrdersAPI/Program.cs
@@ -6,8 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataAccessLayer();
 builder.Services.AddBusinessLogicLayer();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
