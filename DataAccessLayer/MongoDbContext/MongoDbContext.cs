fdb7bfac6d2baa70112ca5a72793377cceb4bf2e
Elect-Trick <snmajenje@gmail.com>
2026-07-07 18:51:42 +0200

    Add Mongo DB context

--- DataAccessLayer/MongoDbContext/MongoDbContext.cs
+++ DataAccessLayer/MongoDbContext/MongoDbContext.cs
@@ -10,7 +10,7 @@ public class MongoDbContext
     {
         var client = new MongoClient(connectionString);
         _database = client.GetDatabase(databaseName);
-        Console.WriteLine($"✅ MongoDB connection successful to {databaseName}");
+        Console.WriteLine($"MongoDB connection successful to {databaseName}");
     }
     
     public IMongoCollection<Order> Orders => _database.GetCollection<Order>("Orders");
