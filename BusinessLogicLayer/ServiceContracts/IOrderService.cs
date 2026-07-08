7525b3cacd29ef447e2adc922b41ff80d972fa69
Elect-Trick <snmajenje@gmail.com>
2026-07-08 22:24:30 +0200

    Update function paramlist

--- BusinessLogicLayer/ServiceContracts/IOrderService.cs
+++ BusinessLogicLayer/ServiceContracts/IOrderService.cs
@@ -1,10 +1,10 @@
 using BusinessLogicLayer.Entities;
-
+using DataAccessLayer.DTOs;
 namespace BusinessLogicLayer.ServiceContracts;
 
 public interface IOrderService 
 {
-    public Task<Order?> CreateOrder(Order order);
+    public Task<Order?> CreateOrder(OrderDto order);
     Task<Order?> GetOrderById(Guid id);
     Task<List<Order>> GetOrders();
     Task<bool> DeleteOrder(Guid id);
