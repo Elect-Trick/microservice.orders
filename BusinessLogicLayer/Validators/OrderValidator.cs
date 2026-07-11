9ce3c1ee056df562275177dc948430ccd8c4d909
Elect-Trick <snmajenje@gmail.com>
2026-07-11 22:28:34 +0200

    Order Item sub items validation added.

--- BusinessLogicLayer/Validators/OrderValidator.cs
+++ BusinessLogicLayer/Validators/OrderValidator.cs
@@ -9,6 +9,12 @@ public class OrderValidator : AbstractValidator<OrderDto>
     {
         RuleFor(order => order.UserId).NotNull().NotEmpty().WithMessage("UserId is required");
         RuleFor(order => order.OrderItems).NotNull().NotEmpty().WithMessage("Order needs to have at least one product");
+        RuleForEach(order => order.OrderItems)
+            .Must(item => item.Quantity > 0).WithMessage("Quantity must be greater than 0")
+            .Must(item => item.ProductId != Guid.Empty).WithMessage("ProductId is required")
+            .Must(item => item.ProductName != string.Empty).WithMessage("ProductName is required")
+            .Must(item => item.ProductName != null ).WithMessage("ProductName is required")
+            .Must(item => item.UnitPrice > 0 ).WithMessage("UnitPrice must be greater than 0");
         
         
     }
