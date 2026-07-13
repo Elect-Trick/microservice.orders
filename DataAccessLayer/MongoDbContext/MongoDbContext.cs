82a4a169482d19ae7246a6958d3357a5d79b5d40
Elect-Trick <snmajenje@gmail.com>
2026-07-13 11:56:39 +0200

    Add ENV config for mongo to docker file and source code.

--- DataAccessLayer/MongoDbContext/MongoDbContext.cs
+++ DataAccessLayer/MongoDbContext/MongoDbContext.cs
@@ -8,9 +8,9 @@ public class MongoDbContext
    private readonly IMongoDatabase _database;
     public MongoDbContext(string connectionString, string databaseName)
     {
-        var client = new MongoClient(connectionString);
+        
+        var client = new MongoClient(connectionString.Replace("$MONGO_DB_HOST",Environment.GetEnvironmentVariable("MONGO_DB_HOST") ?? "localhost").Replace("$MONGO_DB_PORT", Environment.GetEnvironmentVariable("MONGO_DB_PORT") ?? "27017"));
         _database = client.GetDatabase(databaseName);
-        Console.WriteLine($"MongoDB connection successful to {databaseName}");
     }
     
     public IMongoCollection<Order> Orders => _database.GetCollection<Order>("Orders");
