a8ebc0340958c0ef92f5b81caf223c1828b79f11
Elect-Trick <snmajenje@gmail.com>
2026-07-13 11:56:53 +0200

    Add HttpRedirections

--- OrdersAPI/Program.cs
+++ OrdersAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddValidatorsFromAssemblyContaining<OrderValidator>();
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
+app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
