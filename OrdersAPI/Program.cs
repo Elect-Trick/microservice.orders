1ec35c29d0d746207fbb412dd475fe18d68b2438
Elect-Trick <snmajenje@gmail.com>
2026-07-18 17:50:50 +0200

    Inject Polly policy handler (cleanly) into the Program.cs

--- OrdersAPI/Program.cs
+++ OrdersAPI/Program.cs
@@ -1,9 +1,12 @@
 using BusinessLogicLayer.DependencyInjection;
 using BusinessLogicLayer.HttpClient;
+using BusinessLogicLayer.Policies;
+using BusinessLogicLayer.PollyContracts;
 using DataAccessLayer.DependencyInjection;
 using OrdersAPI.Middleware;
 using FluentValidation;
 using BusinessLogicLayer.Validators;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,9 @@ builder.Services.AddDataAccessLayer();
 builder.Services.AddHttpClient<UserMicroServiceClient>(client =>
 {
     client.BaseAddress = new Uri($"http://{Environment.GetEnvironmentVariable("UserMSName")}:{Environment.GetEnvironmentVariable("UserMSPort")}/");
+}).AddPolicyHandler((serviceProvicer, request) =>
+{
+    return serviceProvicer.GetRequiredService<IUserMicroServicePolicies>().GetRetryPolicy();
 });
 
 builder.Services.AddHttpClient<ProductMicroServiceClient>(client =>
